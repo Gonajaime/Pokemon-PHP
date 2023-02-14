722aa064c59700a87773944caacae279b73074d1
joseafch <cherinerxd12376@gmail.com>
2023-02-14 12:38:50 +0100

    pokemon list actualizada

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -20,13 +20,13 @@ class PokemonController extends AbstractController{
             ["name"=>"Pikachu", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
                     "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png", "code"=>"0025"],
 
-                    ["name"=>"Bulbasaur", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
+                    ["name"=>"Bulbasaur", "description"=>"Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.",
                     "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", "code"=>"0001"],
 
-                    ["name"=>"ivusaur", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
+                    ["name"=>"ivusaur", "description"=>"Cuando le crece bastante el bulbo del lomo, pierde la capacidad de erguirse sobre las patas traseras.",
                     "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png", "code"=>"0002"],
 
-                    ["name"=>"elvenosaur", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
+                    ["name"=>"elvenosaur", "description"=>"La planta florece cuando absorbe energía solar, lo cual le obliga a buscar siempre la luz del sol. Esta todo amazao el cabron",
                     "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png", "code"=>"0003"]
                 
                 ];
