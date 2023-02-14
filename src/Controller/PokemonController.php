c35512e57c9740a4cf3a36c344ce9f0435dcecb5
joseafch <cherinerxd12376@gmail.com>
2023-02-14 12:35:15 +0100

    pokemon listado

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -14,5 +14,25 @@ class PokemonController extends AbstractController{
 
 
     }
+    #[Route("/pokemons")]
+    public function showPokemons(){
+        $pokemons = [   
+            ["name"=>"Pikachu", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
+                    "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png", "code"=>"0025"],
+
+                    ["name"=>"Bulbasaur", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
+                    "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", "code"=>"0001"],
+
+                    ["name"=>"ivusaur", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
+                    "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png", "code"=>"0002"],
+
+                    ["name"=>"elvenosaur", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
+                    "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png", "code"=>"0003"]
+                
+                ];
+        return $this->render("pokemons\CharactersPokemons.html.twig", ["pokemons"=>$pokemons]);
+
+
+    }
 
 }
