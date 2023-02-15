38b6bc40434b903e6a76f68b69de0111f4caf0b4
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-15 09:14:54 +0000

    doctrine

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -1,38 +1,60 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PokemonController extends AbstractController{
+class PokemonController extends AbstractController
+{
 
-    #[Route("/pokemon")]
-    public function showPokemon(){
-        $pokemon = ["name"=>"Pikachu", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
-                    "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png", "code"=>"0025"];
+    #[Route("/pokemon/{id}")]
+    public function showPokemon(EntityManagerInterface $doctrine, $id)
+    {
+        $repository = $doctrine->getRepository(Pokemon::class);
+        $pokemon = $repository->find($id);
+
         return $this->render("pokemons\showPokemon.html.twig", ["pokemon"=>$pokemon]);
 
 
     }
+
     #[Route("/pokemons")]
-    public function showPokemons(){
-        $pokemons = [   
-            ["name"=>"Pikachu", "description"=>"Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.",
-                    "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png", "code"=>"0025"],
+    public function showPokemons(EntityManagerInterface $doctrine)
+    {
+        $repository = $doctrine->getRepository(Pokemon::class);
+        $pokemons = $repository->findAll();
 
-                    ["name"=>"Bulbasaur", "description"=>"Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.",
-                    "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", "code"=>"0001"],
-
-                    ["name"=>"ivusaur", "description"=>"Cuando le crece bastante el bulbo del lomo, pierde la capacidad de erguirse sobre las patas traseras.",
-                    "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png", "code"=>"0002"],
-
-                    ["name"=>"elvenosaur", "description"=>"La planta florece cuando absorbe energía solar, lo cual le obliga a buscar siempre la luz del sol. Esta todo amazao el cabron",
-                    "img"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png", "code"=>"0003"]
-                
-                ];
         return $this->render("pokemons\CharactersPokemons.html.twig", ["pokemons"=>$pokemons]);
+    }
 
+    #[Route("/insert/pokemon")]
+    public function insertPokemons(EntityManagerInterface $doctrine)
+    {
+        $pokemon1 = new Pokemon ();
+        $pokemon1->setName("Pikachu");
+        $pokemon1->setDescription("Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.");
+        $pokemon1->setImg("https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png");
+    
+        $pokemon2 = new Pokemon ();
+        $pokemon2->setName("Bulbasaur");
+        $pokemon2->setDescription("Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.");
+        $pokemon2->setImg("https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png");
 
+        $pokemon3 = new Pokemon ();
+        $pokemon3->setName("Pikachu");
+        $pokemon3->setDescription("Cuando se enfada, este Pokémon descarga la energía que almacena en el interior de las bolsas de las mejillas.");
+        $pokemon3->setImg("https://assets.pokemon.com/assets/cms2/img/pokedex/full/032.png");
+
+        $doctrine->persist($pokemon1);
+        $doctrine->persist($pokemon2);
+        $doctrine->persist($pokemon3);
+
+        $doctrine->flush();
+
+        return new Response("Pokemons inserted correctly");
     }
 
 }
